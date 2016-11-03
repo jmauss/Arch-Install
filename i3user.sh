279b97e1b396a6cf2702dfcf7f2edb5d53f34a79
John Mauss <jmauss@users.noreply.github.com>
2016-11-03 15:48:19 -0500

    Add script for adding users in i3

---
+++ i3user.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function ask_for_username()
+{
+while [ 1 ]; do
+        read -p "Enter your name: " name;
+        read -p "Enter your username: " user_name;
+        if [ $user_name ]; then
+                break;
+        fi
+done
+}
+
+function ask_for_password()
+{
+while [ 1 ]; do
+        stty -echo
+        read -p "Preferred password: " passwd1; echo
+        read -p "Enter the password again: " passwd2; echo
+        stty echo
+        if [ "$passwd1" == "$passwd2" ]; then
+                break;
+        else
+                echo "Passwords do not match. Please try again.";
+        fi
+done
+}
+
+if ping -c 1 google.com &> /dev/null
+then
+  echo Connected
+else
+  echo "Not Connected" && dhcpcd && sleep 2m
+fi
+
+ask_for_username
+ask_for_password
+
+useradd -c $name -m -g wheel -s /bin/zsh $user_name
+passwd $user_name << EOPF
+$passwd1
+$passwd2
+EOPF
+
+xdg-user-dirs-update
+
+mkdir -p /home/$user_name/Builds
+mkdir -p /home/$user_name/.config/cower
+mkdir -p /home/$user_name/.config/termite
+mkdir -p /home/$user_name/.config/i3blocks/scripts
+mkdir -p /home/$user_name/.config/i3
+mkdir -p /home/$user_name/.config/gtk-3.0
+
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.zshrci3 -o /home/$user_name/.zshrc
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.Xresources -o /home/$user_name/.Xresources
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.xinitrc -o /home/$user_name/.xinitrc
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.zprofile -o /home/$user_name/.zprofile 
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/termite/config -o /home/$user_name/.config/termite/config
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/gtk-3.0/gtk.css -o /home/$user_name/.config/gtk-3.0/gtk.css
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3blocks/i3blocks.conf -o /home/$user_name/.config/i3blocks/i3blocks.conf
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3blocks/scripts/playing -o /home/$user_name/.config/i3blocks/scripts/playing
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3blocks/scripts/sp -o /home/$user_name/.config/i3blocks/scripts/sp
+curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3/config -o /home/$user_name/.config/i3/config
+cd /home/$user_name/
+cp /usr/share/doc/cower/config .config/cower/
+sed -i 's/#TargetDir =/TargetDir = ~\/Builds\//' .config/cower/config
+chown -R $user_name:wheel .zshrc .Xresources .xinitrc .zprofile .config
+cd .config/i3blocks/scripts/
+chmod u+x playing sp
+cd
+
+rm /home/$user_name/.bash*
+
+shutdown -r now
