e603ba014aa4e594f72c0d676384270869bf7dac
John Mauss <jmauss@users.noreply.github.com>
2016-11-03 14:51:37 -0500

    Setup cower - begin removal of pacaur

--- posti3.sh
+++ posti3.sh
@@ -54,11 +54,11 @@ cd /tmp
 sudo -u $user_name git clone https://aur.archlinux.org/cower.git
 cd cower/
 sudo -u $user_name makepkg -sri --noconfirm --skippgpcheck
-cd /tmp
+#cd /tmp
 
-sudo -u $user_name git clone https://aur.archlinux.org/pacaur.git
-cd pacaur/
-sudo -u $user_name makepkg -sri --noconfirm
+#sudo -u $user_name git clone https://aur.archlinux.org/pacaur.git
+#cd pacaur/
+#sudo -u $user_name makepkg -sri --noconfirm
 cd
 
 sudo -u $user_name pacaur -Syu --noedit --noconfirm
@@ -79,10 +79,13 @@ curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/mirrorupgrade.
 systemctl enable thermald.service
 systemctl enable NetworkManager.service
 
+mkdir -p /home/$user_name/Builds
+mkdir -p /home/$user_name/.config/cower
 mkdir -p /home/$user_name/.config/termite
 mkdir -p /home/$user_name/.config/i3blocks/scripts
 mkdir -p /home/$user_name/.config/i3
 mkdir -p /home/$user_name/.config/gtk-3.0
+
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.zshrci3 -o /home/$user_name/.zshrc
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.Xresources -o /home/$user_name/.Xresources
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.xinitrc -o /home/$user_name/.xinitrc
@@ -94,6 +97,8 @@ curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3block
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3blocks/scripts/sp -o /home/$user_name/.config/i3blocks/scripts/sp
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3/config -o /home/$user_name/.config/i3/config
 cd /home/$user_name/
+cp /usr/share/doc/cower/config .config/cower/
+sed -i 's/#TargetDir =/TargetDir = ~\/Builds\//' .config/cower/config
 chown -R $user_name:wheel .zshrc .Xresources .xinitrc .zprofile .config
 cd .config/i3blocks/scripts/
 chmod u+x playing sp
