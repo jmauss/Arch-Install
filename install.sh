92efc2e06883bee23a6297d0ab903243948b96a3
John Mauss <jmauss@users.noreply.github.com>
2017-01-31 16:55:04 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -199,7 +199,7 @@ cryptloader_bios()
     sed -i "s/use_lvmetad = 1/use_lvmetad = 0/" /mnt/etc/lvm/lvm.conf
     arch-chroot /mnt pacman -S intel-ucode grub os-prober --noconfirm
     arch-chroot /mnt grub-install --target=i386-pc --recheck "$GRUB"
-    sed -i "s#GRUB_CMDLINE_LINUX=\"\"#GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$DEVIDC:lvm\"#" /mnt/etc/default/grub
+    sed -i "s#GRUB_CMDLINE_LINUX=\"\"#GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$DEVIDC:luks\"#" /mnt/etc/default/grub
     arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
