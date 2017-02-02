8ebb521391be03cfd3eed93cc73f8c5e1b7badaa
John Mauss <jmauss@users.noreply.github.com>
2017-02-02 15:22:40 -0600

    Enable pacaur post installation

--- posti3.sh
+++ posti3.sh
@@ -99,9 +99,7 @@ sudo sed -i "\$aQT_QPA_PLATFORMTHEME=qt5ct" /etc/environment
 sed -i 's/Adwaita/Numix-Circle,Adwaita/' /usr/share/icons/Paper/index.theme
 systemctl enable thermald.service
 
-mkdir -p /home/$user_name/Builds
 mkdir -p /home/$user_name/Scripts
-mkdir -p /home/$user_name/.config/cower
 mkdir -p /home/$user_name/.config/termite
 mkdir -p /home/$user_name/.config/i3blocks/scripts
 mkdir -p /home/$user_name/.config/i3
@@ -120,15 +118,12 @@ curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3block
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3blocks/scripts/sp -o /home/$user_name/.config/i3blocks/scripts/sp
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3/config -o /home/$user_name/.config/i3/config
 cd /home/$user_name/
-cp /usr/share/doc/cower/config .config/cower/
-sed -i 's/#TargetDir =/TargetDir = ~\/Builds\//' .config/cower/config
-chown -R $user_name:wheel i3dark.crx .zshrc .Xresources .xinitrc .zprofile .config Builds Scripts
+chown -R $user_name:wheel i3dark.crx .zshrc .Xresources .xinitrc .zprofile .config Scripts
 chmod u+x Scripts/i3user.sh
 cd .config/i3blocks/scripts/
 chmod u+x playing sp
 cd
 
-pacman -Rns pacaur --noconfirm
 pacman -Rns $(pacman -Qqdt) --noconfirm
 
 rm /home/$user_name/.bash*
