682aeb470302dcb7ca927fa3f41e4b9bb068f1ee
John Mauss <jmauss@users.noreply.github.com>
2017-02-03 09:21:11 -0600

    Update i3user.sh

--- i3user.sh
+++ i3user.sh
@@ -63,9 +63,7 @@ $passwd1
 $passwd2
 EOPF
 
-mkdir -p /home/$user_name/Builds
 mkdir -p /home/$user_name/Scripts
-mkdir -p /home/$user_name/.config/cower
 mkdir -p /home/$user_name/.config/termite
 mkdir -p /home/$user_name/.config/i3blocks/scripts
 mkdir -p /home/$user_name/.config/i3
@@ -84,9 +82,7 @@ curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3block
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3blocks/scripts/sp -o /home/$user_name/.config/i3blocks/scripts/sp
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/config/i3/config -o /home/$user_name/.config/i3/config
 cd /home/$user_name/
-cp /usr/share/doc/cower/config .config/cower/
-sed -i 's/#TargetDir =/TargetDir = ~\/Builds\//' .config/cower/config
-chown -R $user_name:wheel i3dark.crx .zshrc .Xresources .xinitrc .zprofile .config Builds Scripts
+chown -R $user_name:wheel i3dark.crx .zshrc .Xresources .xinitrc .zprofile .config Scripts
 chmod u+x Scripts/i3user.sh
 cd .config/i3blocks/scripts/
 chmod u+x playing sp
