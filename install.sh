0b83ca1f68f25aaa97639e9d4b448863d19f91f5
John Mauss <jmauss@users.noreply.github.com>
2017-05-14 14:24:23 -0500

    Add intel video driver

--- install.sh
+++ install.sh
@@ -218,7 +218,7 @@ cryptloader_uefi()
 
 laptop_utilities()
 {
-    arch-chroot /mnt pacman -S iw wpa_supplicant dialog tlp bluez bluez-utils networkmanager mesa xf86-input-libinput xorg-server --noconfirm
+    arch-chroot /mnt pacman -S iw wpa_supplicant dialog tlp bluez bluez-utils networkmanager mesa xf86-video-intel xf86-input-libinput xorg-server --noconfirm
     arch-chroot /mnt systemctl enable tlp.service
     arch-chroot /mnt systemctl enable tlp-sleep.service
     arch-chroot /mnt systemctl disable systemd-rfkill.service
