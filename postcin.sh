9ca735e7d2955f21dbed1bb9f23a3611d86058e4
John Mauss <11668753+jmauss@users.noreply.github.com>
2018-10-29 08:26:23 -0500

    Change AUR helper

--- postcin.sh
+++ postcin.sh
@@ -50,29 +50,29 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 pacman -Syu git --noconfirm --needed
 cd /tmp
 
-sudo -u $user_name git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
+sudo -u $user_name git clone https://aur.archlinux.org/yay.git
+cd yay/
 sudo -u $user_name makepkg -sric --noconfirm --needed
 cd
 
-sudo -u $user_name pikaur -Syu --noedit --noconfirm --needed
+sudo -u $user_name yay -Syu --noedit --noconfirm --needed
 
 # System Core
-sudo -u $user_name pikaur -S downgrade ntfs-3g dosfstools unzip p7zip thermald --noconfirm --noedit --needed
+sudo -u $user_name yay -S downgrade ntfs-3g dosfstools unzip p7zip thermald --noconfirm --noedit --needed
 
 # Audio Drivers
-sudo -u $user_name pikaur -S alsa-utils pulseaudio pulseaudio-alsa --noconfirm --noedit --needed
+sudo -u $user_name yay -S alsa-utils pulseaudio pulseaudio-alsa --noconfirm --noedit --needed
 
 # Cinnamon Core
-sudo -u $user_name pikaur -S ttf-roboto ttf-roboto-mono ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji gtkhash --noconfirm --noedit --needed
+sudo -u $user_name yay -S ttf-roboto ttf-roboto-mono ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji gtkhash --noconfirm --noedit --needed
 
-sudo -u $user_name pikaur -S cinnamon gtkhash-nemo nemo-fileroller lightdm-settings xorg-xrandr eog gnome-{calculator,disk-utility,font-viewer,keyring,screenshot,system-log,system-monitor,terminal} xdg-user-dirs-gtk gedit blueberry system-config-printer --noconfirm --noedit --needed
+sudo -u $user_name yay -S cinnamon gtkhash-nemo nemo-fileroller lightdm-settings xorg-xrandr eog gnome-{calculator,disk-utility,font-viewer,keyring,screenshot,system-log,system-monitor,terminal} xdg-user-dirs-gtk gedit blueberry system-config-printer --noconfirm --noedit --needed
 
 # System Programs
-sudo -u $user_name pikaur -S plank --noconfirm --noedit --needed
+sudo -u $user_name yay -S plank --noconfirm --noedit --needed
 
 # System Theming 
-sudo -u $user_name pikaur -S arc-gtk-theme paper-icon-theme-git papirus-icon-theme-git numix-circle-icon-theme-git lib32-fontconfig qt4 qt5-styleplugins qt5ct --noconfirm --noedit --needed
+sudo -u $user_name yay -S arc-gtk-theme paper-icon-theme-git papirus-icon-theme-git numix-circle-icon-theme-git lib32-fontconfig qt4 qt5-styleplugins qt5ct --noconfirm --noedit --needed
 
 sed -i "\$aQT_QPA_PLATFORMTHEME=qt5ct" /etc/environment
 sed -i 's/Adwaita/Papirus-Dark,Numix-Circle,Adwaita/' /usr/share/icons/Paper/index.theme
@@ -83,8 +83,8 @@ systemctl enable lightdm.service
 curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.zshrc -o /home/jmauss/.zshrc
 chown $user_name:wheel /home/jmauss/.zshrc
 
-sudo -u $user_name pikaur -Rns $(pikaur -Qqdt) --noconfirm
-sudo -u $user_name pikaur -Sc --noconfirm
+sudo -u $user_name yay -Yc --noconfirm
+sudo -u $user_name yay -Sc --noconfirm
 
 rm /home/$user_name/.bash*
 rm -r *
