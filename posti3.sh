4f40b61c616bd35ff09b1e3ec7899a9f65d31376
John Mauss <11668753+jmauss@users.noreply.github.com>
2019-04-02 11:15:15 -0500

    Add gnome themes

--- posti3.sh
+++ posti3.sh
@@ -72,7 +72,7 @@ sudo -u $user_name yay -S mpv --noconfirm --needed # Graphical
 # Themeing
 sudo -u $user_name yay -S qt4 qt5-styleplugins qt5ct lxappearance-gtk3 --noconfirm --needed # Theme engines
 sudo -u $user_name yay -S ttf-roboto nerd-fonts-roboto-mono ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome-4 --noconfirm --needed # Fonts
-sudo -u $user_name yay -S papirus-icon-theme --noconfirm --needed # Themes
+sudo -u $user_name yay -S gnome-themes-extra papirus-icon-theme --noconfirm --needed # Themes
 
 cd /tmp
 sudo -u $user_name git clone https://github.com/alexanderjeurissen/ranger_devicons
