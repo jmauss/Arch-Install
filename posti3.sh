4e468c9b98301abf84e1abc818ced090f59dae3a
John Mauss <11668753+jmauss@users.noreply.github.com>
2019-04-02 14:30:42 -0500

    Remove clean menu from yay

--- posti3.sh
+++ posti3.sh
@@ -53,7 +53,7 @@ sudo -u $user_name makepkg -sric --noconfirm --needed
 cd
 
 sudo -u $user_name yay -Syu --noedit --noconfirm --needed
-sudo -u $user_name yay --editmenu --nodiffmenu --combinedupgrade --save
+sudo -u $user_name yay --editmenu --nodiffmenu --combinedupgrade --nocleanmenu --save
 
 # System Core
 sudo -u $user_name yay -S alsa-utils pulseaudio pulseaudio-alsa --noconfirm --needed # Audio
