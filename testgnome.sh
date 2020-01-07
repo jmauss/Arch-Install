9d6c26a73f5e5974e547aee597fd9e0c73b277d6
John Mauss <11668753+jmauss@users.noreply.github.com>
2020-01-07 08:42:27 -0600

    Update testgnome.sh

--- testgnome.sh
+++ testgnome.sh
@@ -16,6 +16,7 @@ getUsername()
 pacman -Syu zsh zsh-completions git --noconfirm
 
 # Add user as a member of the group wheel with zsh as the default shell
+echo "Create an admin account..."
 getUsername
 useradd -c $name -m -g wheel -s /bin/zsh $user_name
 passwd $user_name
@@ -35,15 +36,14 @@ sudo -u $user_name yay --editmenu --nodiffmenu --combinedupgrade --nocleanmenu -
 sudo -u $user_name yay -Syu --noconfirm --needed
 
 # Gnome Core
-sudo -u $user_name yay -S file-roller gdm gnome-{backgrounds,control-center,keyring,session,settings-daemon,shell,shell-extensions,themes-extra,grilo-plugins} mousetweaks mutter nautilus networkmanager sushi xdg-user-dirs-gtk --noconfirm --needed # Desktop environment
-sudo -u $user_name yay -S gnome-shell-extension-{activities-config,dash-to-dock,remove-dropdown-arrows-git,topicons-plus-git,weather-git} --noconfirm --needed # Shell extensions
-sudo -u $user_name yay -S fonts-roboto-ttf liberation-fonts-ttf noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji --noconfirm --needed # Fonts
+sudo -u $user_name yay -S chrome-gnome-shell file-roller gdm gnome-{backgrounds,control-center,keyring,session,settings-daemon,shell,shell-extensions,themes-extra} grilo-plugins mousetweaks mutter nautilus networkmanager sushi xdg-user-dirs-gtk --noconfirm --needed # Desktop environment
+sudo -u $user_name yay -S ttf-roboto ttf-roboto-mono ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm --needed # Fonts
 sudo -u $user_name yay -S papirus-icon-theme --noconfirm --needed # Theming
 
 # Utils
-sudo -u $user_name yay -S baobab dconf-editor eog evince gedit gnome-{calculator,calendar,disk-utility,font-viewer,logs,maps,screenshot,system-monitor,terminal,tweaks} totem --noconfirm --needed # Gnome
-sudo -u $user_name yay -S virtualbox virtualbox-host-modules-arch virtualbox-ext-oracle papirus-icon-theme --noconfirm --needed # Virtualization
-sudo -u $user_name yay -S ntfs-3g dosfstools exfat-utils unzip p7zip unrar --noconfirm --needed # Files/Filesystems
+sudo -u $user_name yay -S baobab dconf-editor eog gedit gnome-{calculator,calendar,disk-utility,font-viewer,logs,maps,screenshot,system-monitor,terminal,tweaks} totem --noconfirm --needed # Gnome
+sudo -u $user_name yay -S virtualbox-host-modules-arch virtualbox virtualbox-ext-oracle papirus-icon-theme --noconfirm --needed # Virtualization
+sudo -u $user_name yay -S ntfs-3g exfat-utils p7zip unrar --noconfirm --needed # Files/Filesystems
 sudo -u $user_name yay -S downgrade neofetch --noconfirm --needed # Command Line
 
 # Services
@@ -55,6 +55,14 @@ curl https://raw.githubusercontent.com/jmauss/Arch-Install/master/.zshrc -o /hom
 
 # Set up folders
 xdg-user-dirs-update
+
+# Fix Shortcuts
+mkdir -p /home/$user_name/.local/share/applications
+cp /usr/share/applications/{avahi-discover,bssh,bvnc,org.gnome.Cheese,qv4l2,qvidcap}.desktop /home/$user_name/.local/share/applications/
+echo "NoDisplay=true" | tee -a /home/$user_name/.local/share/applications/*.desktop
+
+# Fix permissions
+chown -R $user_name:wheel /home/$user_name/
 
 # Cleanup
 sudo -u $user_name yay -Yc --noconfirm
